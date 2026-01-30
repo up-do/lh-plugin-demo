5bafe5cf66955a31dbb6f18f23d083058b62daeb
Phil de Joux <philderbeast@gmail.com>
2026-01-30 16:52:20 -0500

    Configure updo for ghc-9.6.3

---
+++ project-dhall/ghc-9.6.3/text-templates/stack-snippet.dhall
@@ -0,0 +1,7 @@
+\(_stackage-resolver : Optional Text) ->
+  ''
+  user-message: "This stack project is generated."
+
+  nix:
+    packages: [cacert, git, hostname, z3]
+  ''
--- project-versions.mk
+++ project-versions.mk
@@ -1,7 +1,11 @@
 # Versions of GHC and stackage resolver, the ones we're on and the next ones
 # we're upgrading to.
-GHC_VERSION ?= 9.2.8
-STACKAGE_VERSION ?= lts-20.26
+# GHC_VERSION ?= 9.2.8
+# STACKAGE_VERSION ?= lts-20.26
+# GHC_VERSION ?= 9.4.7
+# STACKAGE_VERSION ?= lts-21.21
+GHC_VERSION ?= 9.6.3
+STACKAGE_VERSION ?= lts-22.6
 
 # For the upgrade, pick a matching pair of ghc-version and stack resolver.
 GHC_UPGRADE ?= 9.2.8
